15d7d6d9e541b6f50b404e2d629d9beceac27aa3
Mayqiyue <mayqiyue@qq.com>
2018-03-09 15:21:20 +0800

    Fix animation disable automatilly when view did disappear

--- OCSkeleton/Classes/OCGradientLayer.m
+++ OCSkeleton/Classes/OCGradientLayer.m
@@ -13,26 +13,23 @@ NSString *kSlidingAnimationKey = @"OCSkeleton.SlidingAnimation";
 
 - (void)slideToDir:(OCDirection)direction animations:(void (^)(CAAnimationGroup *))group
 {
-    if ([self animationForKey:kSlidingAnimationKey]) {
-        return;
-    }
-    
     OCGradientTransition startPointTransition = transitionMaker(direction, OCGradientPropertyStartPoint);
     OCGradientTransition endPointTransition = transitionMaker(direction, OCGradientPropertyEndPoint);
     
     CABasicAnimation *startPointAnim = [CABasicAnimation animationWithKeyPath:@"startPoint"];
     startPointAnim.fromValue = [NSValue valueWithCGPoint:startPointTransition.start];
     startPointAnim.toValue = [NSValue valueWithCGPoint:startPointTransition.end];
-
+    
     CABasicAnimation *endPointAnim = [CABasicAnimation animationWithKeyPath:@"endPoint"];
     endPointAnim.fromValue = [NSValue valueWithCGPoint:endPointTransition.start];
     endPointAnim.toValue = [NSValue valueWithCGPoint:endPointTransition.end];
-
+    
     CAAnimationGroup *animGroup = [[CAAnimationGroup alloc] init];
     animGroup.animations = @[startPointAnim, endPointAnim];
     animGroup.duration = 1;
     animGroup.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
     animGroup.repeatCount = HUGE_VALF;
+    animGroup.removedOnCompletion = false;
     
     if (group) {
         group(animGroup);
@@ -46,3 +43,4 @@ NSString *kSlidingAnimationKey = @"OCSkeleton.SlidingAnimation";
 }
 
 @end
+
